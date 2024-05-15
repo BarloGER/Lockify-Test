5d576b9a43b8fafafc1eff378061778161f19c92
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-05-15 13:07:55 +0200

    Add handlers
    - Add errorHanlder with language support
    - Add asyncHandler to catch errors in routes

---
+++ server/src/interface-adapters/middlewares/errorHandler.js
@@ -0,0 +1,75 @@
+const { errorCodes, errorTypes, statusMessages } = require("../utils");
+const LogRepository = require("../repositories/LogRepository");
+
+const logErrorToDatabase = async ({
+  name,
+  message,
+  statusCode,
+  statusMessage,
+  errorType,
+  errorCode,
+  stack,
+  devInfo,
+  timestamp,
+}) => {
+  const data = {
+    name,
+    message,
+    statusCode,
+    statusMessage,
+    errorType,
+    errorCode,
+    stack,
+    devInfo,
+    timestamp,
+  };
+  const saveLog = await LogRepository.saveLog(data);
+  if (!saveLog) {
+    console.error("Error while saving logs");
+  }
+};
+
+exports.errorHandler = async (err, req, res, next) => {
+  const language = req.headers["accept-language"] === "de" ? "DE" : "EN";
+  const errorCode = err.errorCode || "UNHANDLED_ERROR";
+  const errorCodeInfo = errorCodes[errorCode] || {
+    statusCode: 500,
+    errorType: errorTypes.ServerError,
+    message: {
+      EN: "An unknown error occurred",
+      DE: "Ein unbekannter Fehler ist aufgetreten",
+    },
+  };
+
+  const statusCode = errorCodeInfo.statusCode;
+  const errorType = errorCodeInfo.errorType;
+  const statusMessage = statusMessages[statusCode] || "Server Error";
+  const message = err.validationMessages || errorCodeInfo.message[language];
+
+  const clientResponse = {
+    message: message,
+    statusCode: statusCode,
+    statusMessage: statusMessage,
+    errorType: errorType,
+    errorCode: errorCode,
+  };
+
+  if (process.env.NODE_ENV === "development") {
+    clientResponse.error = { stack: err.stack };
+  }
+
+  res.status(statusCode).json(clientResponse);
+
+  if (process.env.NODE_ENV === "production") {
+    await logErrorToDatabase({
+      message,
+      statusCode,
+      statusMessage,
+      errorType,
+      errorCode,
+      stack: err.stack || "No stack available",
+      devInfo: err.devInfo || {},
+      timestamp: new Date().toISOString(),
+    });
+  }
+};
---
+++ server/src/interface-adapters/utils/asyncHandler.js
@@ -0,0 +1,5 @@
+exports.asyncHandler = (fn) => {
+  return function (req, res, next) {
+    return Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
