a313db241621426b1bc640632c9852ccbb233ea0
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-05-27 09:44:29 +0200

    Add joi validation for routes
    - Add Joi dependency
    - Add validateJoi middleware
    - Add user schemas
    - Add validation messages to error handler

--- server/src/frameworks/web/routes/userRouter.js
+++ server/src/frameworks/web/routes/userRouter.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
 const {
+  validateJoi,
+} = require("../../../interface-adapters/middlewares/validateJoi");
+const {
+  registerUserSchema,
+  loginUserSchema,
+} = require("../../../interface-adapters/joiSchemas/userSchemas");
+const {
   registerUser,
+  loginUser,
 } = require("../../../interface-adapters/controllers/userController");
 
 const userRouter = Router();
 
-userRouter.post("/register", registerUser);
+userRouter.post("/register", validateJoi(registerUserSchema), registerUser);
+userRouter.post("/login", validateJoi(loginUserSchema), loginUser);
 
 exports.userRouter = userRouter;
---
+++ server/src/interface-adapters/joiSchemas/userSchemas.js
@@ -0,0 +1,49 @@
+const Joi = require("joi");
+const {
+  usernameMessages,
+  emailMessages,
+  passwordMessages,
+  unknownObjectMessage,
+} = require("../../utils");
+
+exports.registerUserSchema = (language = "EN") => {
+  return Joi.object({
+    params: Joi.valid({}),
+    body: Joi.object({
+      username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(20)
+        .required()
+        .messages(usernameMessages[language]),
+      email: Joi.string()
+        .email()
+        .max(254)
+        .required()
+        .messages(emailMessages[language]),
+      password: Joi.string()
+        .min(8)
+        .max(20)
+        .required()
+        .messages(passwordMessages[language]),
+    }),
+  }).messages(unknownObjectMessage[language]);
+};
+
+exports.loginUserSchema = (language = "EN") => {
+  return Joi.object({
+    params: Joi.valid({}),
+    body: Joi.object({
+      email: Joi.string()
+        .email()
+        .max(254)
+        .required()
+        .messages(emailMessages[language]),
+      password: Joi.string()
+        .min(8)
+        .max(20)
+        .required()
+        .messages(passwordMessages[language]),
+    }),
+  }).messages(unknownObjectMessage[language]);
+};
