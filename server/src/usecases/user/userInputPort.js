966cf535f003d60fc85f70a3ae5415a97680f2ff
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-05-27 09:48:44 +0200

    Add login functionality and language support for response
    - Add login user controller function
    - Add authenticate user method to user interactor
    - Add crud methods to user repository
    - Add authenticate user method to user input port
    - Add userId to user output port
    - Add comparePasswordHash method to password hashing service using bcrypt

--- server/src/usecases/user/userInputPort.js
+++ server/src/usecases/user/userInputPort.js
@@ -1,16 +1,27 @@
 const { UserEntity } = require("../../entities/User");
+const { ErrorResponse, errorCodes } = require("../../utils");
 
 exports.UserInputPort = class UserInputPort {
-  createUser(user) {
-    return new UserEntity({
-      username: user.username,
-      email: user.email,
-      password: user.password,
-      isVerified: false,
-      isBlocked: false,
-      isNewsletterAllowed: false,
-      verificationCode: null,
-      verificationAttempts: 0,
+  createUser(userInput) {
+    const user = new UserEntity(userInput);
+    const validationError = user.validateForRegistration();
+    if (validationError) {
+      throw new ErrorResponse({ errorCode: `${validationError}` });
+    }
+
+    return user;
+  }
+
+  authenticateUser(userInput) {
+    const credentials = new UserEntity({
+      email: userInput.email,
+      password: userInput.password,
     });
+    const validationError = credentials.validateForLogin();
+    if (validationError) {
+      throw new ErrorResponse({ errorCode: `${validationError}` });
+    }
+
+    return credentials;
   }
 };
