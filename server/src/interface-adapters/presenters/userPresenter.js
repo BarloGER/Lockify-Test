c369306b7da1bf48d1a1e5e12336944010e318cb
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-06-16 16:56:12 +0200

    Add getUser function, reworked filterSensitiveData, minor changes
    - Add getUser() to UserInteractor
    - Change UserOutputPort
    - Change filterSensitiveData
    - Add filterSensitiveData to UserPresenter
    - Add getUser() to userController
    - Fix language in userController
    - Add MailRepository to userController
    - Add routes to userRouter

--- server/src/interface-adapters/presenters/userPresenter.js
+++ server/src/interface-adapters/presenters/userPresenter.js
@@ -1,5 +1,26 @@
-// Will only output if success is true for now
+const { filterSensitiveData } = require("../../utils/filterSensitiveData");
+//! Will only output if success is true for now
+//! Check safeUserData
 exports.UserPresenter = class UserPresenter {
+  presentUser(language, output) {
+    if (!output.success) {
+      return { error: true, message: output.message };
+    }
+
+    const safeUserData = filterSensitiveData(output.user, [
+      "password",
+      "verificationCode",
+      "verificationAttempts",
+      "lastVerificationAttempt",
+    ]);
+
+    return {
+      success: true,
+      message: output.message[language],
+      user: safeUserData,
+    };
+  }
+
   present(language, output) {
     if (!output.success) {
       return { error: true, message: output.message };
@@ -7,7 +28,6 @@ exports.UserPresenter = class UserPresenter {
     return {
       success: true,
       message: output.message[language],
-      userId: output.userId,
     };
   }
 };
--- server/src/usecases/user/userOutputPort.js
+++ server/src/usecases/user/userOutputPort.js
@@ -2,17 +2,26 @@ exports.UserOutputPort = class UserOutputPort {
   constructor() {
     this.success = false;
     this.message = {};
-    this.userId = null;
+    this.user = {};
+  }
+
+  userOutput(data) {
+    this.success = data.success;
+    this.message = data.message;
+    this.user = data.user;
+    return {
+      success: this.success,
+      message: this.message,
+      user: this.user,
+    };
   }
 
   output(data) {
     this.success = data.success;
     this.message = data.message;
-    this.userId = data.userId;
     return {
       success: this.success,
       message: this.message,
-      userId: this.userId,
     };
   }
 };
