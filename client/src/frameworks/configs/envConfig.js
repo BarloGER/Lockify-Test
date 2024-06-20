f2c95ce3d316155e12e1e37c88f739a01d10b646
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-06-20 18:13:29 +0200

    Add configs
    - Add envConfig
    - Add translations
    - Invoke validateEnvs in main.jsx

---
+++ client/src/frameworks/configs/envConfig.js
@@ -0,0 +1,13 @@
+export const validateEnvs = () => {
+  const requiredEnvs = [
+    "VITE_API_KEY",
+    "VITE_MAILSERVER_URL",
+    "VITE_MAIL_ADDRESS",
+  ];
+
+  requiredEnvs.forEach((key) => {
+    if (!import.meta.env[key]) {
+      throw new Error(`Environment variable ${key} is not defined.`);
+    }
+  });
+};
