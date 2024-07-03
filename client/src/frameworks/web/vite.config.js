5f00eece773e9b9355585e20291e78b5cfdff7cd
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-07-03 19:15:21 +0200

    Test docker container

--- client/src/frameworks/web/vite.config.js
+++ client/src/frameworks/web/vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
+  base: "/",
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
 });
