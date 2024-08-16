bc3db0d5275e97602b8a0de17fa074e42f5ea99d
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-08-16 13:58:29 +0200

    Fix MailRepository body
    - Change body html from mailEntity.message to mailEntity.html

--- server/src/interface-adapters/repositories/MailRepository.js
+++ server/src/interface-adapters/repositories/MailRepository.js
@@ -2,6 +2,7 @@ const URL = process.env.MAILSERVER_URL;
 
 exports.MailRepository = class MailRepository {
   async sendMail(mailEntity) {
+    console.log(mailEntity);
     try {
       const response = await fetch(`${URL}/mail/send-mail`, {
         method: "POST",
@@ -11,7 +12,7 @@ exports.MailRepository = class MailRepository {
         body: JSON.stringify({
           email: mailEntity.email,
           subject: mailEntity.subject,
-          html: mailEntity.message,
+          html: mailEntity.html,
         }),
       });
 
